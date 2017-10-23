3e0dfd5edeea9146b40fdf5926164f87f76ff3ac
dt4422 <danitorres2244@gmail.com>
2017-10-23 13:14:11 +0200

    Extracting SIF with Select Button

--- rpath_app/server.R
+++ rpath_app/server.R
@@ -12,51 +12,69 @@ library(shinyFiles)
  Sys.setenv(http_proxy="http://proxy.wifi.uma.es:3128/")
 
 
-#createLink <- function(val) {
- # sprintf('<a href="%s" target="_blank" class="btn btn-default">GO</a>', val)
-#}
+ 
+ createLink <- function(val) {
+   sprintf('<a href="%s" target="_blank" class="btn btn-default">GO</a>', val)
+ }
 
-createImg <- function(val) {
-  url_img <- paste(gsub("http://pathwaycommons.org/pc2/","",val), ".png", sep="")
-  sprintf('<img src="%s" class="img-responsive"></img>', url_img)
-}
+ 
+ createImg <- function(val) {
+   db <- gsub("http://pathwaycommons.org/pc2/","",val)
+   url_img <- paste(db, ".png", sep="")
+   sprintf('<img src="%s" class="img-rounded" alt="%s"></img>', url_img, db)
+ }
 
-shinyServer(function(input, output) {
-  
-  getResultsDf <- eventReactive(input$searchButton,{
-    # Check if input is empty
-    if (is.null(input$term) | is.null(input$dataSources)) return()
-    if (input$numberOfResults<0 | input$numberOfResults>100) return()
-    
-    # Loading bar
-    withProgress(message = 'Loading', value = 0, {
-      incProgress(0.1, detail = paste("Searching on selected databases..."))
-      searchResults <- searchPc(q = input$term, 
-                                datasource = paste(input$dataSources, sep="&"), 
-                                type = "Pathway", organism = input$organism)
-      
-      incProgress(0.5, detail = paste("Parsing results..."))
-      searchResultsDf <- ldply(xmlToList(searchResults), data.frame) 
-      
-      incProgress(0.8, detail = paste("Simplifying results..."))
-      searchResultsDf <- subset(searchResultsDf, .id=="searchHit") # we only keep searchHits (NOT .attrs)
-      headSearchResultsDf <- head(searchResultsDf,  input$numberOfResults) 
-    
-      # If we have searchHit>=1 then create links, images and 
-      # return a subset of the original dataframe
-      if("uri" %in% colnames(headSearchResultsDf))
-      {
-        #headSearchResultsDf$uri <- createLink(headSearchResultsDf$uri)
-        headSearchResultsDf$dataSource <- createImg(headSearchResultsDf$dataSource)
-        headSearchResultsDf <- headSearchResultsDf[, c("name", "dataSource", "numParticipants", "numProcesses", "size", "uri")]
-      }
-      
-      incProgress(1.0, detail = paste("Done!"))
-      finalSearchResultsDf <<- headSearchResultsDf # global variable
-    })
-    
-    return(finalSearchResultsDf)
-  })
+ shinyServer(function(input, output) {
+   
+   getResultsDf <- eventReactive(input$searchButton,{
+     term <- input$term 
+     dataSources <- input$dataSources
+     organism <- input$organism
+     numberOfResults <- input$numberOfResults
+     
+     # Validation
+     if (term == ""){
+       showNotification("No term provided. Executing example query...", type="error")
+       term <- "name:gl?coly*"
+     }
+     if (is.null(dataSources)){
+       showNotification("No datasource selected. Searching on Reactome...", type="error")
+       dataSources <- "reactome"
+     }
+     if (numberOfResults<0 || numberOfResults>100){
+       showNotification("No valid number of results range (1:100). Showing 10 results...", type="error")
+       numberOfResults <- 10
+     }
+     
+     # Loading bar
+     withProgress(message = 'Loading', value = 0, {
+       incProgress(0.1, detail = paste("Searching on selected databases..."))
+       searchResults <- searchPc(q = term, 
+                                 datasource = paste(dataSources, sep="&"), 
+                                 type = "Pathway", organism = organism)
+       
+       incProgress(0.5, detail = paste("Parsing results..."))
+       searchResultsDf <- ldply(xmlToList(searchResults), data.frame) 
+       
+       incProgress(0.8, detail = paste("Simplifying results..."))
+       searchResultsDf <- subset(searchResultsDf, .id=="searchHit") # we only keep searchHits (NOT .attrs)
+       headSearchResultsDf <- head(searchResultsDf,  numberOfResults) 
+       
+       # If we have searchHit>=1 then create links, images and 
+       # return a subset of the original dataframe
+       if("uri" %in% colnames(headSearchResultsDf))
+       {
+         headSearchResultsDf$uri <- createLink(headSearchResultsDf$uri)
+         headSearchResultsDf$dataSource <- createImg(headSearchResultsDf$dataSource)
+         headSearchResultsDf <- headSearchResultsDf[, c("name", "dataSource", "numParticipants", "numProcesses", "size", "uri")]
+       }
+       
+       incProgress(1.0, detail = paste("Done!"))
+       finalSearchResultsDf <<- headSearchResultsDf # global variable
+     })
+     
+     return(finalSearchResultsDf)
+   })
   
   output$searchResults <-  renderDataTable({
     return(getResultsDf())
@@ -64,9 +82,7 @@ shinyServer(function(input, output) {
   )
   
   getRowFromDf <- eventReactive(input$searchResults_rows_selected,{
-    
-    as.character(finalSearchResultsDf$uri[input$searchResults_rows_selected])
-    
+    strsplit(as.character(finalSearchResultsDf$uri[input$searchResults_rows_selected]), "\"")[[1]][2]
   })
   
   
@@ -91,27 +107,22 @@ shinyServer(function(input, output) {
     URI <- as.factor(finalSearchResultsDf$uri[input$searchResults_rows_selected])
     withProgress(message = 'Extracting information', value = 0, {
       
-      incProgress(0.1, detail = paste("ploting..."))
+      incProgress(0.1, detail = paste("Path selected..."))
       #OWL <- getPc(URI,"BIOPAX")
       biopaxFile <- tempfile(tmpdir = getwd(),fileext = ".owl")
       saveXML(getPc(URL), biopaxFile)
       
       
       
-      sif <- toSif("prueba3.owl")
+      sif <- toSif(biopaxFile)
       #g <- graph.edgelist(as.matrix(sif[, c(1, 3)]), directed = FALSE)
     })
     
-    return("OWL")
+    return(sif)
     
   })
   
-  output$roto2 <- renderPrint(
-    
-    
-   ExtractSif()
-    
-  )
+  
   
   
   
--- rpath_app/ui.R
+++ rpath_app/ui.R
@@ -2,18 +2,38 @@ library(shiny)
 library(DT)
 library(shinyjs)
 shinyUI(
-  
-  
-  fluidPage(theme = "bootstrap.min.css",
-            
-    navbarPage("rPath",
+  fluidPage(theme = "css/bootstrap.min.css",
+            # Bootstrap.js
+            tags$script(src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"),
+            # Scripts for background
+            tags$script(src="js/particles.min.js"),
+            tags$script("particlesJS.load('particles-js', 'js/particles.json', function() {
+                        console.log('callback - particles.js config loaded');
+                        });"),
+    tags$style(" #particles-js { 
+               width: 100%; height: 100%; background-image: url(\"\"); 
+               position: fixed; z-index: -10; top: 0; left: 0; }"),
+    tags$div(id="particles-js"),
+    # Script for tooltip
+    tags$script("$(document).ready(function() { $(\"body\").tooltip({ selector: '[data-toggle=tooltip]' });});"),
+    
+    # Page
+    navbarPage(title="rPath",
                tabPanel("Search",
                         fluidRow(
                           column(3,
                                  wellPanel(
                                    textInput("term", "Term:",
-                                             value = "name:gl?coly*"),
-                                   helpText("Term can be a", a("Lucene query string", href="https://lucene.apache.org/core/2_9_4/queryparsersyntax.html", target="_blank")),
+                                             placeholder = "name:gl?coly*"),
+                                   helpText("Consider using a", a("Lucene query", href="https://lucene.apache.org/core/2_9_4/queryparsersyntax.html", target="_blank"),
+                                            "string. Here you have some ",
+                                            HTML('<a data-toggle="collapse" data-target="#ex">examples</a>.'),
+                                            tags$div(id = 'ex',  class="collapse",
+                                                     tags$li(a(id="example1","gly*",  `data-toggle`="tooltip", `data-placement`="right", title="Search in KEGGs all the pathways that start with gly- followed by whatever")),
+                                                     tags$li(a(id="example2", "met?b*",  `data-toggle`="tooltip", `data-placement`="right", title="Search in KEGGs all the pathways that start with met- followed by whatever")),
+                                                     tags$li(a(id="example3", "gluc*",`data-toggle`="tooltip", `data-placement`="right", title="Search in KEGGs all the pathways that start with gluc- followed by whatever"))
+                                            )
+                                   ),
                                    textInput("organism", "Organism",
                                              value = "9606"),
                                    helpText("e.g. \"homo sapiens\", \"9606\""),
@@ -27,19 +47,18 @@ shinyUI(
                                                                   "DrugBank" = "drugbank",
                                                                   "INOH" = "inoh"),
                                                       selected = c("kegg", "reactome", "panther", "inoh")),
-                                   
                                    numericInput("numberOfResults", "No. of results:", 
                                                 value = 10),
                                    helpText("Minimum 0, maximum 100"),    
-
+                                   
                                    div(class="text-center", actionButton("searchButton", "Search", class="btn-primary"))
-                                 ),
-                                 wellPanel(
-                                   fileInput("fileToUpload", "Or, alternativity, upload a file:",
-                                             multiple = FALSE,
-                                             accept = c(".owl")),
-                                   div(class="text-center", actionButton("uploadButton", "Upload", class="btn-primary"))
                                  )
+                                 #wellPanel(
+                                 # fileInput("fileToUpload", "Or, alternativity, upload a file:",
+                                 #           multiple = FALSE,
+                                 #           accept = c(".owl")),
+                                 # div(class="text-center", actionButton("uploadButton", "Upload", class="btn-primary"))
+                                 #)
                           ),
                           column(9,
                                  span("Search results:"),
@@ -52,19 +71,28 @@ shinyUI(
                                  ),
                                  useShinyjs(),
                                  
-                                 actionButton("Plotme", "Plot"),
+                                 actionButton("Plotme", "Select")
                                  
-                                 span("Pueba:"),
-                                 wellPanel(
-                                   verbatimTextOutput("roto2")
-                                 )
+                                 
                                  
                                  
                                  
                           )
                         )),
-               tabPanel("tab2"),
-               tabPanel("tab3")
+               tabPanel("Visualization",
+                        absolutePanel(class="controls panel panel-default draggable ui-draggable ui-draggable-handle", 
+                                      style="background-color: white; padding: 0 20px 20px 20px; cursor: move;
+                                      opacity: 0.65; zoom: 0.9; transition: opacity 500ms 1s;", fixed = TRUE,
+                                      draggable = TRUE, top = 80, left = "auto", right = 20, bottom = "auto",
+                                      width = 330, height = "auto",
+                                      
+                                      h2("Example"),
+                                      
+                                      textInput("a", "A"),
+                                      textInput("b", "B")
+                        )),
+               tabPanel("Analysis"),
+               tabPanel("User Manual")
     )
     
   )
